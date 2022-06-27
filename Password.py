e74db264c80a0a87d48c0a53cfd6a6b895727760
vaibhav <vaibhavghuge198@gmail.com>
2022-06-27 13:43:17 +0530

    first changes

---
+++ Password.py
@@ -0,0 +1,37 @@
+#To calculate stdent total marks and grade
+print("To calculate stdent total marks and grade")
+obt_marks=float(input("enter obtaind marks ="))
+total_marks=float(input("enter total marks ="))
+percentage=(obt_marks/total_marks)*100
+if percentage<35:
+    print("fail")
+   
+elif percentage<45:
+        print("Grade C")
+elif percentage<60:
+            print("Grade B")
+elif percentage<75:
+                print("Grade A")
+else:
+                print("Grade A++")
+
+print(percentage)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
---
+++ my_programs.py
@@ -0,0 +1,39 @@
+#to write a program for store student data and using iterable operator over a dictionry
+
+'''std_data={ 1:["vaibhav",27],2:["tushar",24],3:["ganesh",45],4:["raj",20]}
+for x,y in std_data.items():
+    print(x,y)
+    
+    
+range(1,100)
+for i in range(1,101):
+    print(i*2,end=" ")
+    
+a=int(input("enter value of first side of triangle A= "))
+b=int(input("enter value of first side of triangle B= "))
+if a==b:
+    c=a**2+b**2
+    print(c**2)
+    
+a=["automobiles","honda","benz","suzuki","forrd"]
+
+b=[len(i) for i in a]
+#print(b)
+for i in a:
+    b.append(i)
+print(b) 
+
+    
+string=("shshshsksjjs")
+count =map(lambda x:x=='s',string) #Type your code here
+
+cout=list(count)
+print(len(cout))'''
+
+
+num1= float(input("enter any 1st no ="))
+num2= float(input("enter any 2st no = "))
+
+sum=num1+num2
+print("tow number of addition =",sum)
+    
